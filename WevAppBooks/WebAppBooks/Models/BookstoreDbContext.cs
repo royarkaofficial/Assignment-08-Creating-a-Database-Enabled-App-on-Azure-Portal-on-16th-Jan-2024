2f2458bc7e70e85901388cb688632f991541f916
Arkadeep Roy <royarkadeep1112@gmail.com>
2024-01-17 07:18:02 +0530

    Assignment

---
+++ WevAppBooks/WebAppBooks/Models/BookstoreDbContext.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebAppBooks.Models
+{
+    public partial class BookstoreDbContext : DbContext
+    {
+        public BookstoreDbContext()
+        {
+        }
+
+        public BookstoreDbContext(DbContextOptions<BookstoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; } = null!;
+        public virtual DbSet<Book> Books { get; set; } = null!;
+        public virtual DbSet<BookCategory> BookCategories { get; set; } = null!;
+        public virtual DbSet<Publisher> Publishers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:servernirmal11.database.windows.net,1433;Initial Catalog=BookstoreDb;Persist Security Info=False;User ID=nirmalraj;Password=Nirmaldb@15;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>(entity =>
+            {
+                entity.ToTable("Author");
+
+                entity.Property(e => e.AuthorId).ValueGeneratedNever();
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.ToTable("Book");
+
+                entity.Property(e => e.BookId).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Books)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK__Book__CategoryId__628FA481");
+
+                entity.HasOne(d => d.Publisher)
+                    .WithMany(p => p.Books)
+                    .HasForeignKey(d => d.PublisherId)
+                    .HasConstraintName("FK__Book__PublisherI__6383C8BA");
+
+                entity.HasMany(d => d.Authors)
+                    .WithMany(p => p.Books)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "BookAuthor",
+                        l => l.HasOne<Author>().WithMany().HasForeignKey("AuthorId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK__BookAutho__Autho__6754599E"),
+                        r => r.HasOne<Book>().WithMany().HasForeignKey("BookId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK__BookAutho__BookI__66603565"),
+                        j =>
+                        {
+                            j.HasKey("BookId", "AuthorId").HasName("PK__BookAuth__6AED6DC4A11CBBED");
+
+                            j.ToTable("BookAuthor");
+                        });
+            });
+
+            modelBuilder.Entity<BookCategory>(entity =>
+            {
+                entity.HasKey(e => e.CategoryId)
+                    .HasName("PK__BookCate__19093A0BFE8E9205");
+
+                entity.ToTable("BookCategory");
+
+                entity.Property(e => e.CategoryId).ValueGeneratedNever();
+
+                entity.Property(e => e.CategoryName).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Publisher>(entity =>
+            {
+                entity.ToTable("Publisher");
+
+                entity.Property(e => e.PublisherId).ValueGeneratedNever();
+
+                entity.Property(e => e.Address).HasMaxLength(50);
+
+                entity.Property(e => e.PublisherName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- obj/Release/net6.0/WebAppPlayers.AssemblyInfo.cs
+++ obj/Release/net6.0/WebAppPlayers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppPlayers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52999dfdc2d5fe5274603478a99c5df54b36cece")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppPlayers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppPlayers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
